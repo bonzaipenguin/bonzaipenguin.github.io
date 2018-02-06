ecbeff6c453a1cff4ed295a57624bdaa44172109
Jerusha <jerusha@creativelive.com>
2018-02-06 14:25:16 -0800

    Adding error handling

--- endorserNav.js
+++ endorserNav.js
@@ -1,4 +1,5 @@
 $('document').ready(function(){
+  
   $('.add').on('click',function(){
     if ($('.opened').hasClass('ninth')) {
       $('.opened').next('.multi-form').removeClass('hidden').addClass('opened');
@@ -12,4 +13,30 @@ $('document').ready(function(){
     }
   });
 
+  var createAllErrors = function() {
+    var form = $(this),
+      errorList = $('ul.errorMessages', form);
+    var showAllErrorMessages = function() {
+      errorList.empty();
+      var invalidFields = form.find(':invalid').each(function(index, node) {
+        var label = $('label[for=' + node.id + '] '),
+          message = node.validationMessage || 'Invalid value.';
+        errorList.show().append('<li><span>' + label.html() + '</span> ' + message + '</li>');
+      });
+    };
+    form.on('submit', function(event) {
+      if (this.checkValidity && !this.checkValidity()) {
+        $(this).find(":invalid").first().focus();
+        event.preventDefault();
+      }
+    });
+    $('input[type=submit]', form).on('click', showAllErrorMessages);
+    $('input', form).on('keypress', function(event) {
+      var type = $(this).attr('type');
+      if (/date|email|month|number|search|tel|text|time|url|week/.test(type) && event.keyCode == 13) {
+        showAllErrorMessages();
+      }
+    });
+  };
+  $('form').each(createAllErrors);
 });
