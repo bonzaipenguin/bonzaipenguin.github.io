adcd9ee07d5cab50683f2e5c172881c8916212c8
Jerusha Johnson <jerusha@Jerushas-MacBook-Pro.local>
2018-03-02 17:51:14 -0500

    Organization

--- js/model.js
+++ js/model.js
@@ -1,7 +1,14 @@
 define([], function() {
 
 var model = {
-	projectList:[{}]
+	projecList:[
+		{type:'personal',dir:'/spanish-cards',name:'Spanish Test'},
+		{type:'work',dir:'/tbl-apps/landing-page',name:'C200 Landing Page'},
+		{type:'work',dir:'/tbl-apps/sign-up/c200',name:'C200 Sign Up Flow'},
+		{type:'work',dir:'/tbl-apps/sign-up/obw',name:'OnBoarding Women Sign Up Flow'},
+		{type:'work',dir:'/tbl-apps/sign-up/tu',name:'TIMES\'S UP  Sign Up Flow'},
+		{type:'work',dir:'/tbl-apps/til-home',name:'theInvestlist Homepage'}
+	]
 };
 return model;
 });
