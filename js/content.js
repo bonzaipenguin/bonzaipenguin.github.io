cc0c3c6ce6a9a7eada01284213a9b9055b7ec120
Jerusha Johnson <jerusha@Jerushas-MacBook-Pro.local>
2018-03-05 22:21:54 -0500

    Styling and added the _.where function

--- js/content.js
+++ js/content.js
@@ -4,31 +4,44 @@ define(['jquery','model'], function($,model) {
 		ui:$('<section class="row"></section>'),
 		mine:$('<div class="col-sm"></div>'),
 		land:$('<div class="col-sm"></div>'),
-		sign:$('<div class="col-sm"></div>'),
+		temp:$('<div class="col-sm"></div>'),
+		work:$('<div class="col-sm"></div>'),
 		myHead:$('<h3>Personal Work</h3>'),
 		landHead:$('<h3>Landing Pages</h3>'),
-		signHead:$('<h3>Sign Up Flows</h3>'),
+		tempHead:$('<h3>Sign Up Flows</h3>'),
+		workHead:$('<h3>Prototypes</h3>'),
 		myList:$('<ul class="project-list list-unstyled" data-type="personal"></ul>'),
-		landList:$('<ul class="project-list list-unstyled" data-type="work"></ul>'),
-		signList:$('<ul class="project-list list-unstyled" data-type="work"></ul>'),
-		projs:function(type,group,url,name){
-			var item = $('<li class="project-item" data-type="'+type+'" data-group"'+group+'"></li>');
+		landList:$('<ul class="project-list list-unstyled" data-type="landing"></ul>'),
+		tempList:$('<ul class="project-list list-unstyled" data-type="temp"></ul>'),
+		workList:$('<ul class="project-list list-unstyled" data-type="proto"></ul>'),
+		proj:function(type,url,name){
+			var item = $('<li class="project-item" data-type="'+type+'"></li>');
 			var link = $('<a href="'+url+'" target="_self"></a>');
 			var title = $('<p>'+name+'</p>');
-
-			if(group == null){console.log('hidden');}
 
 			item.append(link.append(title));
 
 			return item;
 		}
 	};
-	content.ui.append(content.mine).append(content.land).append(content.sign);
+	content.ui.append(content.mine).append(content.land).append(content.temp).append(content.work);
 	content.mine.append(content.myHead).append(content.myList);
 	content.land.append(content.landHead).append(content.landList);
-	content.sign.append(content.signHead).append(content.signList);
-	model.projectList.forEach(function(i){
-		content.myList.append(new content.projs(i.type,i.group,i.url,i.name));
+	content.temp.append(content.tempHead).append(content.tempList);
+	content.work.append(content.workHead).append(content.workList);
+	_.where(model.projectList).forEach(function(i){
+		if(i.type === 'personal'){
+			content.myList.append(new content.proj(i.type,i.url,i.name));
+		}
+		if(i.type === 'landing'){
+			content.landList.append(new content.proj(i.type,i.url,i.name));
+		}
+		if(i.type === 'temp'){
+			content.tempList.append(new content.proj(i.type,i.url,i.name));
+		}
+		if(i.type === 'proto'){
+			content.workList.append(new content.proj(i.type,i.url,i.name));
+		}
 	});
 
 	return content;
--- js/model.js
+++ js/model.js
@@ -3,11 +3,11 @@ define([], function() {
 var model = {
 	projectList:[
 		{type:'personal',url:'/spanish-cards',name:'Spanish Test'},
-		{type:'work',url:'/tbl-apps/landing-page',name:'C200',group:'Landing Pages'},
-		{type:'work',url:'/tbl-apps/sign-up/c200',name:'C200',group:'Sign Up Flows'},
-		{type:'work',url:'/tbl-apps/sign-up/obw',name:'OnBoarding Women',group:'Sign Up Flows'},
-		{type:'work',url:'/tbl-apps/sign-up/tu',name:'TIMES\'S UP',group:'Sign Up Flows'},
-		{type:'work',url:'/tbl-apps/til-home',name:'theInvestlist Homepage',group:'Sign Up Flows'}
+		{type:'landing',url:'/tbl-apps/landing-page',name:'C200'},
+		{type:'temp',url:'/tbl-apps/sign-up/c200',name:'C200'},
+		{type:'temp',url:'/tbl-apps/sign-up/obw',name:'OnBoarding Women'},
+		{type:'temp',url:'/tbl-apps/sign-up/tu',name:'TIMES\'S UP'},
+		{type:'proto',url:'/tbl-apps/til-home',name:'theInvestlist Homepage'}
 	]
 };
 return model;
