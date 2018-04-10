0c4e74f26513cc8299e7c656fd51875a4298ee88
Jerusha Johnson <jerusha@Jerushas-MacBook-Pro.local>
2018-04-10 12:24:46 -0700

    Finished landing page

--- tbl-apps/landing-page/30percentclub/js/model.js
+++ tbl-apps/landing-page/30percentclub/js/model.js
@@ -5,19 +5,17 @@ var model = {
   bgImg:'https://raw.githubusercontent.com/bonzaipenguin/bonzaipenguin.github.io/master/tbl-apps/landing-page/30percentclub/style/imgs/splash.jpg',
   hook:'BUILDING BETTER BOARDS THROUGH DIVERSITY ',
   ctaBtns:[
-    {name:'GET DISCOVERED',url:'https://theboardlist.com/30percent-application-c'},
     {name:'ENDORSE WOMEN',url:'https://theboardlist.com/30percent-application-e'},
     {name:'DISCOVER WOMEN',url:'https://theboardlist.com/30percent-application-s'}
   ],
   tagline:'The U.S. 30% Club is partnering with theBoardlist to promote its network to companies searching for board&nbsp;candidates.',
   desc:[
-    {name:'GET DISCOVERED',info:'Women nominated to theBoardlist have professionally pre-prepared, customizable board bios on&nbsptheBoardlist.',link:'Update Your Board Profile',url:''},
     {name:'ENDORSE',info:'Individuals with board experience are eligible to become Endorsing Members of theBoardlist and nominate board-ready&nbspwomen.',link:'Recommend Board Candidates',url:''},
     {name:'DISCOVER',info:'Companies may search our exclusive database of endorsed, board-ready women for their open board&nbspseats.',link:'Search for Board Candidates',url:''}
   ],
   learning:[
     {name:'How theBoardlist Works',info:'theBoardlist is a platform for leaders in their industries to nominate women for all stages of company boards. Once a woman is nominated, she\'s discoverable by companies searching for candidates for their open board&nbspseats.',link:'Learn&nbspMore&nbspAbout&nbsptheBoardlist',url:'https://theboardlist.com/'},
-    {name:'About the The U.S. 30% Club',info:'The U.S. 30% Club aims to develop a diverse pool of talent for all businesses through the efforts of its Chair and CEO members who are committed to better gender balance at all levels of&nbsp;their&nbsp;organisations.',link:'Learn More About&nbsp;the&nbsp;U.S.&nbsp;30%&nbsp;Club',url:'https://us.30percentclub.org/'}  //Co-Branding Organization
+    {name:'About The U.S. 30% Club',info:'The U.S. 30% Club aims to develop a diverse pool of talent for all businesses through the efforts of its Chair and CEO members who are committed to better gender balance at all levels of&nbsp;their&nbsp;organisations.',link:'Learn More About&nbsp;the&nbsp;U.S.&nbsp;30%&nbsp;Club',url:'https://us.30percentclub.org/'}  //Co-Branding Organization
   ]
 };
 return model;
