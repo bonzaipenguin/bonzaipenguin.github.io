aa4147fddfca9aa59fb6f2698e867a2847aa2f8a
Jerusha Holsinger <bonzai.penguin@gmail.com>
2024-10-21 19:25:36 -0700

    Update the toolbar on the nav

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,10 +3,10 @@ import * as React from 'react';
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 
 
 export default function Root() {
@@ -22,10 +22,27 @@ export default function Root() {
           variant="permanent"
           anchor="right"
         >
-          <Toolbar />
-          <Divider />
+          <Toolbar>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Jerusha Holsinger
+            </Typography>
+          </Toolbar>
           <List>
-            {['Journeys', 'KQED FM', 'Rich Push & SMS Workflow','Cheetah Digital Redesign'].map((text) => (
+            {['Journeys', 'KQED FM', 'Rich Push & SMS Workflow','Scheduled Journeys'].map((text) => (
               <ListItem key={text} disablePadding>
                 <ListItemButton>
                   <ListItemText primary={text} />
