53f18e7ece90db32d02070d8ac5457aadac9255d
Jerusha Holsinger <bonzai.penguin@gmail.com>
2024-10-22 16:42:23 -0700

    Finish skeleton on website

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
+// import Root from './routes/root.jsx';
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,68 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-import * as React from 'react';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-
-
+/* eslint-disable no-unused-vars */
+import * as React from "react";
+import ResponsiveDrawer from "../assets/components/responsiveDrawer.jsx";
 export default function Root() {
     return (
       <>
-        <Drawer
-          sx={{
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-              boxSizing: 'border-box',
-            },
-          }}
-          variant="permanent"
-          anchor="right"
-        >
-          <Toolbar>
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              Jerusha Holsinger
-            </Typography>
-          </Toolbar>
-          <List>
-            {['Journeys', 'KQED FM', 'Rich Push & SMS Workflow','Scheduled Journeys'].map((text) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-          <Divider/>
-          <List>
-            {['About Me', 'Résumé / CV'].map((text) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <div id="detail"></div>
+        <ResponsiveDrawer></ResponsiveDrawer>
       </>
     );
   }
