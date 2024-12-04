2683f35498cc75e3fea8cefa9322eade452c179b
Jerusha Holsinger <bonzai.penguin@gmail.com>
2024-12-04 13:38:36 -0800

    Add resume

--- app/page.js
+++ app/page.js
@@ -11,11 +11,11 @@ export default function Home() {
         <h2>Designer, Amateur Photographer, Coffee Drinker</h2>
       </header>
         <p>
-          I'm a designer for a little over 10 years, who has worked at every size company from seed level to Fortune 500. I've been the only designer and one of 30. To me, teamwork is key and the best idea always wins - which is usually a hybrid of all the ideas on the table.
+          I&apos;m a designer for a little over 10 years, who has worked at every size company from seed level to Fortune 500. I&apos;ve been the only designer and one of 30. To me, teamwork is key and the best idea always wins - which is usually a hybrid of all the ideas on the table.
         </p>
         <ul>
-          <li><a href="www.linkedin.com/in/jerushaholsinger">LinkedIn</a></li>
-          <li><Link href="/resume">Resume</Link></li>
+          <li><a href="https://www.linkedin.com/in/jerushaholsinger" target="_blank">LinkedIn</a></li>
+          <li><a href="../public/resume_2024.pdf" target="_blank">Resume</a></li>
         </ul>
       </main>
       <footer className={styles.footer}>
