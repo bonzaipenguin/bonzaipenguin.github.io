c7bed5c74aa0aa22e2011c810e91bc08fbebe7ba
Jerusha Holsinger <bonzai.penguin@gmail.com>
2024-12-09 20:32:44 -0800

    Edits to page styles

--- app/page.js
+++ app/page.js
@@ -12,8 +12,10 @@ export default function Home() {
         <p>
           I have over 10 years of design experience, having worked at companies ranging from startups to Fortune 500 corporations. I have experience as both a solo designer and part of a team of 30. Teamwork is essential, as great ideas often emerge from diverse perspectives.
         </p>
-        <Link href="https://www.linkedin.com/in/jerushaholsinger" target="_blank">LinkedIn</Link>
-        <Link href="../public/resume_2024.pdf" target="_blank">Resume</Link>
+        <div>
+          <Link href="https://www.linkedin.com/in/jerushaholsinger" target="_blank">LinkedIn</Link>
+          <Link href="../public/resume_2024.pdf" target="_blank">Resume</Link>
+        </div>
       </main>
       <footer className={styles.footer}>
         <Link href="/scheduled-journeys">Scheduled Journeys</Link>
